c173feff13c8f42b828e65e0ca74db87f959738c
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-04-01 17:29:54 +0200

    trasferimento di axios e link api in main (cambio struttura)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -20,37 +20,11 @@
 <script>
 
 
-import axios from "axios"
+
 export default {
     name: "HeaderIndex",
+    
 
-    data: function(){
-        return{
-            inputText:"",
-            films: null,
-
-            titolo: null,
-            titoloOriginale: null,
-            voto: null,
-            lingua: null,
-        }
-    },
-
-     created: function(){
-        this.apiFilms()
-    },
-
-    methods:{
-        apiFilms(){
-            axios.get("https://api.themoviedb.org/3/movie/550?api_key=5f9d6c871c535668747bc3410bc15999&query=").then((result) =>{
-                this.titolo = result.data.name
-                this.titoloOriginale = result.data.original_title
-                this.voto = result.data.popularity
-                this.lingua = result.data.original_language
-                
-            })
-        }
-    }
 }
 </script>
 
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,12 +8,44 @@
 </template>
 
 <script>
+import axios from "axios"
 import MainContent from './MainContent.vue'
 export default {
     name:"MainIndex",
+
     components:{
-        MainContent
+        MainContent,
+    },
+
+    data: function(){
+        return{
+            inputText:"",
+            films: null,
+
+            titolo: null,
+            titoloOriginale: null,
+            voto: null,
+            lingua: null,
+        }
+    },
+
+    created: function(){
+        this.apiFilms()
+    },
+
+    methods:{
+        apiFilms(){
+            axios.get("https://api.themoviedb.org/3/movie/550?api_key=5f9d6c871c535668747bc3410bc15999&query=").then((result) =>{
+                this.titolo = result.data.name
+                this.titoloOriginale = result.data.original_title
+                this.voto = result.data.popularity
+                this.lingua = result.data.original_language
+                
+            })
+        }
     }
+
+    
 }
 </script>
 
