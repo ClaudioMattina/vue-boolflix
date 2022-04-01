d098f82d1831fbb6ac1e9b47f22725142d655819
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-04-01 12:35:49 +0200

    setting

---
+++ src/components/info.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      5f9d6c871c535668747bc3410bc15999
+
+      https://api.themoviedb.org/3/movie/550?api_key=5f9d6c871c535668747bc3410bc15999
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
