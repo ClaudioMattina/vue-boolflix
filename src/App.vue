e9eb2b639edd0779da8f5a550dc0c307ad6d6ccf
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-04-04 10:31:18 +0200

    chiamata axios nel main

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
 
 <script>
 
+import axios from "axios"
 import Header from './components/Header.vue'
 import Main from './components/Main.vue'
 
@@ -24,16 +25,36 @@ export default {
   data:function(){
     return{
       string: null,
+
+
+      films: null,
+
+      titolo: null,
+      titoloOriginale: null,
+      voto: null,
+      lingua: null,
     }
   },
 
   methods:{
+
+       apiFilms(){
+            axios.get("https://api.themoviedb.org/3/movie/550?api_key=5f9d6c871c535668747bc3410bc15999&query=").then((result) =>{
+                this.titolo = result.data.name
+                this.titoloOriginale = result.data.original_title
+                this.voto = result.data.popularity
+                this.lingua = result.data.original_language
+                
+            })
+        }
+    },
+
     ciao(input){
       console.warn(input)
       this.string = "https://api.themoviedb.org/3/movie/550?api_key=5f9d6c871c535668747bc3410bc15999&query=" + input
     }
   }
-}
+
 </script>
 
 <style lang="scss">
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-import axios from "axios"
+
 import MainContent from './MainContent.vue'
 export default {
     name:"MainIndex",
@@ -22,12 +22,7 @@ export default {
     data: function(){
         return{
             
-            films: null,
-
-            titolo: null,
-            titoloOriginale: null,
-            voto: null,
-            lingua: null,
+            
             
         }
     },
@@ -37,24 +32,17 @@ export default {
     },
 
     methods:{
-        apiFilms(){
-            axios.get("https://api.themoviedb.org/3/movie/550?api_key=5f9d6c871c535668747bc3410bc15999&query=").then((result) =>{
-                this.titolo = result.data.name
-                this.titoloOriginale = result.data.original_title
-                this.voto = result.data.popularity
-                this.lingua = result.data.original_language
-                
-            })
-        }
+     
     }
-
     
 }
 </script>
 
 <style lang="scss" scoped>
+
     main{
         background-color: grey;
         height: 90vh;
     }
+
 </style>
