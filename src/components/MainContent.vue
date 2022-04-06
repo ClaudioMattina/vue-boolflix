d37029961989bb3ee0262a7329a5c7c5431b3a5e
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-04-06 18:38:13 +0200

    milestone 2 finito e problemi col milestone 3

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,11 +1,13 @@
 <template>
 
   <div class="scheda">
-    
+    <div class="img-container">
+      <img src="http://image.tmdb.org/t/p/w500/${infoOggetti.poster_path}" alt="">
+    </div>
     <div>
         <h5> {{infoOggetti.original_title}} {{infoOggetti.name}} </h5>
         <p> {{infoOggetti.title}} </p>
-        <p> {{infoOggetti.original_language}} </p>
+        <p> <lang-flag :iso="`${infoOggetti.original_language}`" /> </p>
         <p> {{infoOggetti.vote_average}} </p>
     </div>
     
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
 
+import LangFlag from 'vue-lang-code-flags';
+
+
+
+Vue.config.productionTip = false
+Vue.component('lang-flag', LangFlag);
 new Vue({
+  
   render: h => h(App),
 }).$mount('#app')
